24e77cc1d4bd0c2696881975b08b6ea7ce3daa45
jakes2255 <aj727in@gmail.com>
2025-06-04 15:21:35 +0530

    database transaction name modified to reflect  custom db changes.

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -6,8 +6,8 @@
   srv.before ('CREATE', 'Orders', async (req) => {
     const order = req.data
     if (!order.amount || order.amount <= 0)  return req.error (400, 'Order at least 1 book')
-    const tx = cds.transaction(req)
-    const affectedRows = await tx.run (
+    const bookshopTransaction  = cds.transaction(req)
+    const affectedRows = await bookshopTransaction .run (
       UPDATE (Books)
         .set   ({ stock: {'-=': order.amount}})
         .where ({ stock: {'>=': order.amount},/*and*/ ID: order.book_ID})
