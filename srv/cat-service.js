c2f25a5a6a0c3f45a9416aaa072cf8b2ff2fac68
jakes2255 <aj727in@gmail.com>
2025-06-16 17:19:57 +0530

    Code Refactoring

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -7,12 +7,12 @@
     const order = req.data
     if (!order.amount || order.amount <= 0)  return req.error (400, 'Order at least 1 book')
     const bookshopTransaction  = cds.transaction(req)
-    const affectedRows = await bookshopTransaction .run (
+    const affectedBooks = await bookshopTransaction .run (
       UPDATE (Publications)
         .set   ({ stock: {'-=': order.amount}})
         .where ({ stock: {'>=': order.amount},/*and*/ ID: order.book_ID})
     )
-    if (affectedRows === 0)  req.error (409, "Sold out, sorry")
+    if (affectedBooks === 0)  req.error (409, "Sold out, sorry")
   })
 
   // Add some discount for overstocked Publications
