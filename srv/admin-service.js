36ccb222658da16efa9b4d1ab85eff041b9f9d59
jakes2255 <aj727in@gmail.com>
2026-02-04 12:55:28 +0530

    Correcting rules for admin service to make app run

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -3,7 +3,7 @@ const cds = require('@sap/cds');
 const { SELECT, UPDATE, DELETE } = cds.ql;
 
 //Consume CAP Rules module
-const determinePrice = require('../rules/price-policy');
+const determinePrice = require('./rules/price-policy');
 
 class AdminService extends cds.ApplicationService {
   async init() {
